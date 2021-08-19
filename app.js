452872ea6b10cff9c8715bd8623ed76e21c8d9d8
VineethKumarM <munigyala@gmail.com>
2021-08-19 10:20:16 +0530

    Basic timetable webpage

---
+++ app.js
@@ -0,0 +1,20 @@
+prompt("HEllo")
+const form = document.querySelector('form');
+const list = document.querySelector('#list');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const assign = form.elements.Aname;
+    const sub = form.elements.sbj;
+    const date = form.elements.date;
+    addItem(assign.value,sub.value,date.value)
+    assign.value=''
+    sub.value = ''
+    date.value = ''
+})
+
+const addItem= (prdt,quan,date) => {
+    const item = document.createElement('li')
+    item.innerText = `${quan} ${prdt} Due on ${date}`;
+    list.appendChild(item)
+}
