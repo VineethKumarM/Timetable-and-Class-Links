17d242d1a1983c794ddf49aab47c00b3a39a6dc1
VineethKumarM <munigyala@gmail.com>
2021-08-25 10:15:52 +0530

    updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Timetable-and-Class-Links
-A basic Webpage which has the Schedule and Links for the Classes
+A basic Webpage which has the Schedule and Links for the Classes(Sec-C)
 [ACIVE LINK OF THE PAGE](https://vineethkumarm.github.io/Timetable-and-Class-Links/)
 
 Few Classes don't have fixed links, so some links are not valid.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 // prompt("HEllo")
 const form = document.querySelector('form');
 const list = document.querySelector('#list');
+// const input = document.querySelector('#Aname');
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
